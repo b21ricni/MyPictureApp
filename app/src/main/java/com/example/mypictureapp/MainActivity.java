2a6c68973cc86f9101016465f6c41fc91ad4e87f
b21ricni <b21ricni@student.his.se>
2022-05-16 13:51:27 +0200

    Added img from third party

--- app/src/main/java/com/example/mypictureapp/MainActivity.java
+++ app/src/main/java/com/example/mypictureapp/MainActivity.java
@@ -3,12 +3,17 @@ package com.example.mypictureapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Picasso.get().load("https://i.imgur.com/DvpvklR.png").into(imageView);
     }
 }
